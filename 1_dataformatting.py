1b06f5979df07fe441a9d811333183fe2c417596
Abraham Traore <abrahamtraore@mac.home>
2026-04-13 19:47:53 +0200

    Add of a new file

---
+++ 1_dataformatting.py
@@ -0,0 +1,9 @@
+from library import Call_Put_processing
+
+address= "/Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/call_put_data"
+nb_obs = 10000 # The model is trained on 10 thousand observations
+processing = Call_Put_processing(address, nb_obs)
+processing.load_and_format()
+processing.save(address, "usa_options")
+
+
