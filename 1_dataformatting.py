33a19686f0e43cc9e71cb54662bbb62144132390
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 15:57:04 +0200

    Code cleaning

--- 1_dataformatting.py
+++ 1_dataformatting.py
@@ -2,13 +2,10 @@ import os
 from library import Call_Put_processing
 
 address= "./call_put_data/"
-
 if os.path.exists(address+"call_usa_options.csv"):
   os.remove(address+"call_usa_options.csv")
-
 if os.path.exists(address+"put_usa_options.csv"):
   os.remove(address+"put_usa_options.csv")
-  
 nb_obs = 10000 
 processing = Call_Put_processing(address, nb_obs)
 processing.load_and_format()
