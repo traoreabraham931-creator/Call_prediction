20b0971cb8a0195623daed4527329dd5fdff2cc6
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 15:51:53 +0200

    Code cleaning

--- 2_model_train_save.py
+++ 2_model_train_save.py
@@ -27,11 +27,7 @@ for elt in curr.description:
     col_names.append(elt[0])
 
 data= data.rename(columns={i:col_names[i] for i in range(len(col_names))})
-
 print(data.head())
-
-#data = pd.read_csv("/app/call_usa_options.csv")
-
 train = data.iloc[:9000,:]
 x_train = train[[col for col in data.columns if col not in ["expiration","strike", "bid_size","ask_size"]]].to_numpy()
 print("The columns for training are {0}".format([col for col in data.columns if col not in ["expiration","strike", "bid_size","ask_size"]]))
@@ -42,13 +38,11 @@ x_train = x_train.astype(np.float32)
 y_train = y_train.astype(np.float32)
 print(x_train.shape)
 print(y_train.shape)
-
 shape = data.shape[1]
 architecture = Architecture(shape)
 architecture.model_recovery()
 architecture.ml_model.fit(x_train, y_train, epochs=100, shuffle=False)
 joblib.dump(architecture.ml_model, 'model_for_inference_gcp.pkl')
-
 # Test of the model
 test = data.iloc[9000:,:]
 x_test = test[[col for col in data.columns if col not in ["expiration","strike", "bid_size","ask_size"]]].to_numpy()
