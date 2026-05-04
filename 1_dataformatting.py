4b6f676b3ded51b52c490d819a3e367f64494608
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 11:04:34 +0200

    Call of the cleaning function

--- 1_dataformatting.py
+++ 1_dataformatting.py
@@ -4,6 +4,7 @@ address= "/Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/
 nb_obs = 10000 # The model is trained on 10 thousand observations
 processing = Call_Put_processing(address, nb_obs)
 processing.load_and_format()
+processing.replace_missing()
 processing.save(address, "usa_options")
 
 
