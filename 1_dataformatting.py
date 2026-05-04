77654c3af4cf677f41b13406eea5b2cea0b31464
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 12:03:45 +0200

    Add of the env file

--- 1_dataformatting.py
+++ 1_dataformatting.py
@@ -1,6 +1,6 @@
 from library import Call_Put_processing
 
-address= "/Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/call_put_data"
+address= "/Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/Programmes/call_put_data/"
 nb_obs = 10000 # The model is trained on 10 thousand observations
 processing = Call_Put_processing(address, nb_obs)
 processing.load_and_format()
--- data_storage_code_launch.sh
+++ data_storage_code_launch.sh
@@ -1,12 +1,13 @@
-docker cp /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/call_put_data/call_usa_options.csv posgre_sql://var/lib/postgresql/data/
+python ./1_dataformatting.py
+docker cp /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/Programmes/call_put_data/call_usa_options.csv posgre_sql://var/lib/postgresql/data/
 # Opening of the database and creation of the table to store the dataset
 docker exec -i posgre_sql \
 psql -U user -d time_series < ./query_file.sql
-docker exec -it call_prediction-python_app-1 python3 2_model_train_save.py
-docker cp call_prediction-python_app-1://app/model_for_inference_gcp.pkl ./
+#docker exec -it call_prediction-python_app-1 python3 2_model_train_save.py
+#docker cp call_prediction-python_app-1://app/model_for_inference_gcp.pkl ./
 #/Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction
-docker cp call_prediction-python_app-1://app/prediction_call_put.npy ./
-docker cp call_prediction-python_app-1://app/test_data.npy /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction
+#docker cp call_prediction-python_app-1://app/prediction_call_put.npy ./
+#docker cp call_prediction-python_app-1://app/test_data.npy /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction
 
 
 
