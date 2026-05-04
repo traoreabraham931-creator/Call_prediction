222df8c3419a325304509130821f7a96481d7613
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 15:00:49 +0200

    Code cleaning

--- data_storage_code_launch.sh
+++ data_storage_code_launch.sh
@@ -1,5 +1,5 @@
 python3 ./1_dataformatting.py
-docker cp /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/Programmes/call_put_data/call_usa_options.csv posgre_sql://var/lib/postgresql/data/
+docker cp ./call_put_data/call_usa_options.csv posgre_sql://var/lib/postgresql/data/
 # Opening of the database and creation of the table to store the dataset
 docker exec -i posgre_sql \
 psql -U user -d time_series < ./query_file.sql
