ba2a44fe1cd69cbc3fe883b465554d201ad59c67
Abraham Traore <abrahamtraore@mac.home>
2026-05-04 10:01:21 +0200

    Add of a comment

--- launch-docker.sh
+++ launch-docker.sh
@@ -4,7 +4,7 @@ docker rm -vf $(docker ps -aq)
 # deletion of all of the existing images
 docker rmi -f $(docker images -aq)
 # creation of the new docker containers
-docker-compose -f /Users/abrahamtraore/Desktop/Python_upskill/Options_strike_prediction/Programmes/docker-compose.yml up
+docker-compose -f ./Programmes/docker-compose.yml up
 
 
 
