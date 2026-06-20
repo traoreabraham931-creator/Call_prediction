bdc04f47b24f105c6454e1f8dde00292a0f07548
Abraham Traore <abrahamtraore@mac.home>
2026-06-20 19:36:19 +0200

    Set up of a specific container name

--- data_storage_code_launch.sh
+++ data_storage_code_launch.sh
@@ -3,11 +3,11 @@ docker cp ./call_put_data/call_usa_options.csv posgre_sql://var/lib/postgresql/d
 # Opening of the database and creation of the table to store the dataset
 docker exec -i posgre_sql \
 psql -U user -d time_series < ./query_file.sql
-docker exec -it call_prediction-python_app-1 python3 2_model_train_save.py
-docker cp call_prediction-python_app-1://app/model_for_inference_gcp.pkl ./output
-docker cp call_prediction-python_app-1://app/custom_attention.weights.h5 ./output
-docker cp call_prediction-python_app-1://app/prediction_call_put.npy ./output
-docker cp call_prediction-python_app-1://app/test_data.npy ./output
+docker exec -it python_app python3 2_model_train_save.py
+docker cp python_app://app/model_for_inference_gcp.pkl ./output
+docker cp python_app://app/custom_attention.weights.h5 ./output
+docker cp python_app://app/prediction_call_put.npy ./output
+docker cp python_app://app/test_data.npy ./output
 
 
 
